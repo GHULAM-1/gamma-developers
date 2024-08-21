db9f88a375fb2e985c68da308f05700a40b49d0a
abubakar-dev-1 <mianabubaka007@gmail.com>
2024-08-21 15:13:48 +0500

    abubakar branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link";
-export default function Home() {
+import Home from "./components/home";
+
+export default function Page() {
   return (
     <>
-      <div className="bg-black w-screen h-screen text-green-500 flex justify-center items-center gap-5 flex-col">
-        <div className="text-7xl">under development</div>
-        <div className="text-white opacity-30">
-          This domain is own by{" "}
-          <Link
-            href="https://github.com/GHULAM-1"
-            target="_blank"
-            className="underline"
-          >
-            GhulamMohi-ud-Din ( Gamma)
-          </Link>
-        </div>
-      </div>
+      <Home/>
     </>
   );
 }
