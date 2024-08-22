5e68f1b6b655a247b71ed2485dbaf0af522a7e4c
IbrahimChaudhary <ibrahimchaudhary422@gmail.com>
2024-08-22 15:30:12 +0500

    pulled

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -10,7 +10,7 @@ export default function NavBar() {
           <Logo/>
           <div>
             <ul className="flex text-white gap-7 border-[1px] px-5 py-2 rounded-full text-sm border-slate-600">
-              <li  className="hover:bg-[#D0FF71] hover:text-black px-4 py-1 cursor-pointer rounded-full" >Home</li>
+              <li  className="hover:bg-[#D0FF71] hover:text-black px-4 py-1 cursor-pointer rounded-full" >Homec</li>
               <li className="hover:bg-[#D0FF71] hover:text-black px-4 py-1 cursor-pointer rounded-full" >Portfolio</li>
               <li className="hover:bg-[#D0FF71] hover:text-black px-4 py-1 cursor-pointer rounded-full" >Blog</li>
               <li className="hover:bg-[#D0FF71] hover:text-black px-4 py-1 cursor-pointer rounded-full" >About us</li>
