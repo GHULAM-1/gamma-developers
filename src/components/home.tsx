dbf1f92e276a3424005901920eca4321920dc7f3
IbrahimChaudhary <ibrahimchaudhary422@gmail.com>
2024-09-03 01:20:05 +0500

    pull

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Hero from "./hero-sec";
-import { DirectionAwareHoverDemo } from "./portfolio-cards";
-import ServicesSec from "./services-sec";
-import BlogSec from "./blog-sec";
-import Footer from "./footer";
-import { TestimonialCarousel } from "./testimonials";
-import { CardSpotlight } from "@/components/ui/card-spotlight";
-import { CanvasRevealEffect } from "./ui/canvas-reveal-effect";
-import StackSec from "./stack-sec";
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <DirectionAwareHoverDemo />
-      <ServicesSec />
-      <StackSec />
-      <BlogSec />
-      <TestimonialCarousel />
-    </>
-  );
-=======
 import React from 'react'
 import Hero from './hero-sec'
 import Stack from './stack-sec'
@@ -42,5 +19,4 @@ export default function Home() {
 
   )
 
->>>>>>> abubakar
 }
--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,16 +1,10 @@
 "use client";
 
 import * as React from "react";
-<<<<<<< HEAD
-import { testimonialsData } from "@/data/testimonilas-data";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useEffect, useState } from "react";
-=======
 import { testimonialsData } from "@/data/testimonials-data";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useEffect, useState } from "react";
 
->>>>>>> abubakar
 export function TestimonialCarousel() {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
@@ -19,51 +13,17 @@ export function TestimonialCarousel() {
       setCurrentIndex(
         (prevIndex: number) => (prevIndex + 1) % testimonialsData.length
       );
-<<<<<<< HEAD
-    }, 3500); 
-=======
     }, 3500); // Change testimonial every 3 seconds
->>>>>>> abubakar
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div className="flex rounded-2xl items-center mt-10 mb-40 py-[100px]  justify-between bg-[#1A1B1E]">
-=======
     <div className="flex items-center justify-between">
->>>>>>> abubakar
       <div className=" mb-9 w-[60%]">
         <img src="/testimonials-img.png" alt="" />
       </div>
 
-<<<<<<< HEAD
-      <div className="transition-all ease-linear w-full flex max-w-[1365px]  justify-center items-center  text-white ">
-        <div className="w-full p-8 rounded ">
-          <p className=" text-[36px] tracking-tighter pb-10 border-b-[1px] border-neutral-400 border-opacity-25">
-            "{testimonialsData[currentIndex].message}"
-          </p>
-          <div className="flex justify-between items-center">
-            <div className="mt-8 text-right font-semibold flex justify-start gap-4 ">
-              <Avatar className="w-[50px] h-[50px]">
-                <AvatarImage src={testimonialsData[currentIndex].img} />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col text-sm items-start">
-                <div className=" text-[18px] mb-2">
-                  {testimonialsData[currentIndex].name}
-                </div>
-                <div className="text-neutral-500 text-[14px]">
-                  {testimonialsData[currentIndex].designation}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-=======
 
     <div className="transition-all ease-linear w-full flex justify-center items-center bg-background text-white ">
       <div className="w-full p-8 rounded shadow-lg">
@@ -96,6 +56,5 @@ export function TestimonialCarousel() {
       </div>
     </div>
     </div>
->>>>>>> abubakar
   );
 }
