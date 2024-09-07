d576c79b4460a2bf9fdb803f6383bbebe5f1eace
IbrahimChaudhary <ibrahimchaudhary422@gmail.com>
2024-09-07 20:40:44 +0500

    ns

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Clock } from "lucide-react";
-
 export default function BlogCard({
   image,
   mainText,
@@ -14,32 +13,34 @@ export default function BlogCard({
 }: BlogType) {
   return (
     <>
-      <div className="w-[365px] flex flex-col gap-5">
+      <div className="w-full max-w-[300px] flex flex-col gap-5">
         <div className="flex justify-between w-full">
           <div className="flex w-full gap-3 ">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarFallback>{ownerName.charAt(0)}</AvatarFallback>
             </Avatar>
             <div className="flex flex-col text-[13px]">
               <div>{ownerName}</div>
               <div className="text-[#FFFFFF9A] font-light">Editor</div>
             </div>
           </div>
-          <div className="flex gap-5  whitespace-nowrap items-center justify-center">
+          <div className="flex gap-5 whitespace-nowrap items-center justify-center">
             <Clock className="text-[#FFFFFF9A] font-light w-[13px] h-[13px]" />
             <span className="text-[#FFFFFF9A] font-light text-[13px]">
               {date}
             </span>
           </div>
         </div>
+
         <Image
           width={365}
           height={255}
           src="https://uithemez.com/i/hubfolio_HTML/inner_pages/assets/imgs/blogs/blog1/1.jpg"
-          alt=""
+          alt={mainText} // Providing descriptive alt text
           className="rounded-2xl"
         />
+
         <div className="text-[22px] font-semibold">{mainText}</div>
       </div>
     </>
--- src/components/blog-sec.tsx
+++ src/components/blog-sec.tsx
@@ -7,11 +7,12 @@ export default function BlogSec() {
   return (
     <>
       <div className="max-w-[1365px] w-full flex flex-col justify-center items-center mb-36">
-        <SectionDivider text="Our Blogs" />
-        <div className="text-white flex justify-start w-full gap-12 flex-wrap ">
+        <SectionDivider text="Our Blogs" className="md:flex hidden"/>
+        <SectionDivider text="Blogs" className="flex md:hidden"/>
+        <div className="text-white flex justify-start w-full gap-12 pt-5 flex-wrap ">
           {blogsDummyData.map((blog: BlogType, index) => {
             return (
-              <div className=" flex flex-row gap-12">
+              <div className=" flex flex-row gap-0 md:gap-12">
                 <BlogCard
                   date={blog.date}
                   image={blog.image}
@@ -22,7 +23,7 @@ export default function BlogSec() {
                   key={index}
                 />
                 {index !== blogsDummyData.length - 1 ? (
-                  <div className="border-[1px] h-full border-neutral-400 border-opacity-20"></div>
+                  <div className="border-none md:border-[1px] h-full border-neutral-400 border-opacity-20"></div>
                 ) : null}
               </div>
             );
