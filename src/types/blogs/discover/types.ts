a8620a3a2538582d7bcac1f97588b12e4a1964e3
SahilIjaz <hssahil2913@gmail.com>
2025-10-30 20:10:22 +0500

    dead code removed

--- src/types/blogs/discover/types.ts
+++ src/types/blogs/discover/types.ts
@@ -13,19 +13,9 @@ export interface DiscoverCard {
   subtitle: string;
   date: string;
   buttonText: string;
-  image: string; // URL string from query
-  slug: string; // ⚠️ This should be string, not { current: string }
+  image: string;
+  slug: string; 
   tags: { _id: string; title: string }[];
 }
 
 
-
-// interface DiscoverCard {
-//   _id: string;
-//   title: string;
-//   subtitle: string;
-//   date: string;
-//   buttonText: string;
-//   image: any;
-//   tags: { _id: string; title: string }[];
-// }
